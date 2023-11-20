9e911d9d978315cf1e565bd7189cca481caa2036
muhwanto0123 <55321001+muhwanto0123@users.noreply.github.com>
2023-11-21 06:23:06 +0700

    [UPDATE] Readme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,36 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
 import { useRouter } from "next/router";
-import { getCookie } from "cookies-next";
+import { deleteCookie, getCookie } from "cookies-next";
+import SweetAlert from "react-bootstrap-sweetalert";
 
 function navbar() {
   const route = useRouter();
   const [user, setUser] = React.useState(null);
+  const [alert, setAlert] = React.useState(null);
+
   React.useEffect(() => {
     if (getCookie("user")) {
       setUser(JSON.parse(getCookie("user")));
     }
   }, []);
+
+  const sweetAlertStyle = { display: "block", marginTop: "-100px" };
+
+  const successAlert = () => {
+    setAlert(
+      <SweetAlert
+        success
+        style={sweetAlertStyle}
+        title="Good job!"
+        onConfirm={() => this.setAlert(null)}
+        onCancel={() => this.setAlert(null)}
+      >
+        You clicked the button!
+      </SweetAlert>
+    );
+  };
+
   return (
     <>
       <nav className="flex justify-between mt-5">
@@ -23,16 +43,19 @@ function navbar() {
         />
         {user ? (
           <div className="pr-[12px] md:pr-[0px]">
-            <button>
-              <img
-                src={
-                  user?.photo ??
-                  "https://www.w3schools.com/w3images/avatar5.png"
-                }
-                width="50px"
-                className="rounded-full"
-              />
-            </button>
+            <div>
+              
+              <button onClick={successAlert}>
+                <img
+                  src={
+                    user?.photo ??
+                    "https://www.w3schools.com/w3images/avatar5.png"
+                  }
+                  width="50px"
+                  className="rounded-full"
+                />
+              </button>
+            </div>
           </div>
         ) : (
           <div className="flex gap-[10px]">
